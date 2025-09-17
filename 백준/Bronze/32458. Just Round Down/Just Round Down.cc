78921a5af48b680d7edca157822e4986f9984054
anozanami <121954367+anozanami@users.noreply.github.com>
2025-09-17 21:25:25 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    double x;
+    cin >> x;
+    ll result = static_cast<ll>(x);
+    cout << result << endl;
+}
+
+/*
+
+*/
