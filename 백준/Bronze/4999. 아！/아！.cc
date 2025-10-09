2e720200765b15a68c56e2191712aae063a4b36a
anozanami <121954367+anozanami@users.noreply.github.com>
2025-10-09 22:02:41 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s1, s2;
+    cin >> s1 >> s2;
+    if (s1.length() >= s2.length()) {
+        cout << "go" << endl;
+    } else {
+        cout << "no" << endl;
+    }
+}
+
+/*
+*/
